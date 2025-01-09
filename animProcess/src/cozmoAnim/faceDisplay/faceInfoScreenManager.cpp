44efbc1ef2cae8a4f361250a635ba7b40ea48cc4
Raj-jyot Singh Deol <deolrajjyot@gmail.com>
2025-01-09 17:35:26 -0500

    Update faceInfoScreenManager.cpp
    Fix that

--- animProcess/src/cozmoAnim/faceDisplay/faceInfoScreenManager.cpp
+++ animProcess/src/cozmoAnim/faceDisplay/faceInfoScreenManager.cpp
@@ -243,15 +243,7 @@ void FaceInfoScreenManager::Init(Anim::AnimContext* context, Anim::AnimationStre
     ADD_SCREEN(Camera, Kercre123);
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  ADD_SCREEN(Kercre123, Main);
-=======
   ADD_SCREEN_WITH_TEXT(Kercre123, Main, {"BRANCH: Victorware-2.0-tester"});
->>>>>>> parent of 158234be67 (Update faceInfoScreenManager.cpp)
-=======
-  ADD_SCREEN(Kercre123, Main, {"BRANCH: Victorware-2.0-tester"});
->>>>>>> parent of 6f0761b13f (Update faceInfoScreenManager.cpp)
 
 
   // ========== Screen Customization ========= 
