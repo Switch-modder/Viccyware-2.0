8b80fe0317a37310e80fd372761cb8ef596a61bd
Switch-modder <deolrajjyot@gmail.com>
2025-01-24 20:51:54 -0500

    Fix paths with spaces when building

--- docker/build-v.sh
+++ docker/build-v.sh
@@ -3,11 +3,11 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
+cd "$DIR"
 
 docker build -t victor .
-docker build -t victor-${USER} -f Dockerfile.dev --build-arg USER=${USER} --build-arg UID=$(id -u $USER) --build-arg VIC_DIR=${DIR}/../ .
+docker build -t victor-"${USER}" -f Dockerfile.dev --build-arg USER="${USER}" --build-arg UID=$(id -u "$USER") --build-arg VIC_DIR="${DIR}/../" .
 docker run \
-       -u ${USER} \
-       -v ~/:/home/${USER}/:delegated \
-       --privileged victor-${USER}
+       -u "${USER}" \
+       -v "~/:/home/${USER}/:delegated \"
+       --privileged victor-"${USER}"
