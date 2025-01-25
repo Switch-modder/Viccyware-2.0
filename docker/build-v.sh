81f0119e1095bd37772a843c0467b95346479f7c
Switch-modder <deolrajjyot@gmail.com>
2025-01-24 21:08:26 -0500

    Missed that

--- docker/build-v.sh
+++ docker/build-v.sh
@@ -9,5 +9,5 @@ docker build -t victor .
 docker build -t victor-"${USER}" -f Dockerfile.dev --build-arg USER="${USER}" --build-arg UID=$(id -u "$USER") --build-arg VIC_DIR="${DIR}/../" .
 docker run \
        -u "${USER}" \
-       -v "~/:/home/${USER}/:delegated \"
+       -v "~/:/home/${USER}/:delegated" \
        --privileged victor-"${USER}"
