16f8172a038e8b6517deb34a7b98ce58fec5513d
Raj-jyot Singh Deol <deolrajjyot@gmail.com>
2025-01-25 15:55:00 -0500

    Enable backpack lights extra track

--- animProcess/src/cozmoAnim/animation/animationStreamer.cpp
+++ animProcess/src/cozmoAnim/animation/animationStreamer.cpp
@@ -362,7 +362,7 @@ namespace Anim {
   CONSOLE_VAR(bool, kShouldDisplayPlaybackTime, "AnimationStreamer", false);
 
   // Disable streaming of backpack lights keyframes by default
-  CONSOLE_VAR(bool, kEnableBackpackLightsTrack, "AnimationStreamer", false);
+  CONSOLE_VAR(bool, kEnableBackpackLightsTrack, "AnimationStreamer", true);
 
   } // namespace
 
