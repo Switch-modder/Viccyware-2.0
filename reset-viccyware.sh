873dd027705c5fa661b9468efe4bdc34fad6054e
Switch-modder <deolrajjyot@gmail.com>
2025-02-21 09:40:17 -0500

    Add stuff to the reset script

--- reset-viccyware.sh
+++ reset-viccyware.sh
@@ -4,3 +4,9 @@ git submodule sync --recursive
 git submodule update --init --force --recursive
 git clean -ffdx
 git submodule foreach --recursive git clean -ffdx
+cd EXTERNALS
+git checkout viccyware
+git reset --hard
+git clean -ffdx
+cd ..
+
