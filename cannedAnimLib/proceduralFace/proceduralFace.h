ca9fe9a74e9d6c75f664a3d5874ebe6d807ffedf
Switch-modder <deolrajjyot@gmail.com>
2025-03-03 20:55:29 -0500

    Make scanlines more accurate

--- cannedAnimLib/proceduralFace/proceduralFace.h
+++ cannedAnimLib/proceduralFace/proceduralFace.h
@@ -327,7 +327,7 @@ inline void ProceduralFace::SetScanlineOpacity(Value opacity)
 
 inline ProceduralFace::Value ProceduralFace::GetScanlineOpacity() const {
 #if PROCEDURALFACE_SCANLINE_FEATURE
-  return 0.0f;
+  return 0.7f;
 #else
   return 1.0f;
 #endif
